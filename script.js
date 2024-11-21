479faee335629e5c4ef7da8aaf12eeed72ae18e5
Nora-H1 <nora.hashani01@gmail.com>
2024-11-21 16:14:12 +0100

    Uptdates files

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// * are walls, . are paths, S is the starting point of the player and T is the treasure you should reach in order to end the game.
-import {LEVEL_1} from "./maze.js";
+import {LEVEL_1, LEVEL_2, LEVEL_3} from "./maze.js";
 
 const mazeClasses = {
     '*': 'walls',
@@ -8,18 +7,32 @@ const mazeClasses = {
     'T': 'treasure'
   };
 
-  // Sélectionne l'élément <main>
+// Sélectionne l'élément <main>
 const main = document.querySelector('main');
 
-// Fonction pour générer un labyrinthe
+//générer un labyrinthe
 function renderMaze(maze) {
   main.innerHTML = ''; // Vide le contenu précédent du <main> 
+  const column = maze[0].length;
+  console.log(column);
 
+  main.style.display = 'grid';
+  main.style.gridTemplateColumns = `repeat(${column},50px)`;
+  main.style.alignItems = 'center';
 
   maze.forEach(row => {
     row.forEach(cell => {
       const div = document.createElement('div');
-      div.textContent = cell;
+      const span = document.createElement('span');
+
+      div.appendChild(span);
+
+      span.textContent = cell;
+      
+      if (['*', '.', 'S', 'T'].includes(cell)) {
+        span.classList.add('hidden-text');
+      }
+      
 
       div.classList.add(mazeClasses[cell] || 'unknown');
 
